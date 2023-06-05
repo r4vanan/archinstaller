513104497ba5dd223d90f0f163cfed5be85faa08
r4vanan <vinothlawrence66@gmail.com>
2023-06-05 13:35:18 +0530

    hostname setup

--- archinstaller.sh
+++ archinstaller.sh
@@ -38,8 +38,7 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 echo "KEYMAP=us" > /etc/vconsole.conf
-echo "Hostname: "
-read hostname
+read -r "hostname?Hostname"
 echo $hostname > /etc/hostname
 echo "127.0.0.1       localhost" >> /etc/hosts
 echo "::1             localhost" >> /etc/hosts
